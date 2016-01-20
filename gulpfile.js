4ee09f890b6314001efa938222f91a4630ff6382
Oleksandr Boiko <mega.boich@gmail.com>
2016-01-20 14:21:35 +0100

    Updates to infrastructure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2');
- 
-gulp.task('default', function () {
-	var tsResult = gulp.src(['src/**/*.ts', 'lib/**/*.ts'])
+var jasmine = require('gulp-jasmine');
+
+gulp.task('release', function () {
+	var tsResult = gulp.src([
+            'src/**/*.ts', 
+            'lib/**/*.ts', 
+            '!src/**/*.spec.ts'
+            ])
 		.pipe(ts({
             declarationFiles: true,
             noExternalResolve: true,
@@ -13,4 +18,28 @@ gulp.task('default', function () {
 		.pipe(gulp.dest('build'));
     
     return tsResult;
+});
+
+ 
+gulp.task('build_tests', function () {
+	var tsResult = gulp.src([
+            'src/**/*.ts', 
+            'lib/**/*.ts'
+            ])
+		.pipe(ts({
+            declarationFiles: true,
+            noExternalResolve: true,
+			noImplicitAny: true,
+			out: 'app_test.js'
+		}))
+		.pipe(gulp.dest('build'));
+    
+    return tsResult;
+});
+
+
+gulp.task('run_tests', ['build_tests'], function () {
+    return gulp.src('build/app_test.js')
+		// gulp-jasmine works on filepaths so you can't have any plugins before it 
+		.pipe(jasmine());
 });
